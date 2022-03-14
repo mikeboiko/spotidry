7b8c13bbb02b66475a35b80b42f74d45059b1a06
Mike Boiko <michaelboiko@gmail.com>
2022-03-14 15:05:00 -0600

    Tested installing spotidry as a CLI

--- spotidry/__init__.py
+++ spotidry/__init__.py
@@ -4,3 +4,4 @@ spotipy
 
 __version__ = '0.0.1'
 __author__ = 'Mike Boiko'
+__email__ = 'mike@boiko.ca'
---
+++ spotidry/cli.py
@@ -0,0 +1,13 @@
+"""Console script for spotidry."""
+
+import sys
+
+
+def main(args=None):
+    """Console script for spotidry."""
+    print('Great success')
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
