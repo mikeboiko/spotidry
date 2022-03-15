758029bdd0ce4ebfb31a2b771032a55a83cb7012
Mike Boiko <michaelboiko@gmail.com>
2022-03-15 11:13:01 -0600

    Created a class

--- spotidry/cli.py
+++ spotidry/cli.py
@@ -3,10 +3,13 @@ Console script for spotidry
 '''
 
 import sys
-from . import spotidry
+from spotidry import Spotidry
+# import spotidry
 
 def main(args=None):
     """Console script for spotidry."""
+    s = Spotidry()
+    s.get_status()
     return 0
 
 if __name__ == "__main__":
--- spotidry/spotidry.py
+++ spotidry/spotidry.py
@@ -7,35 +7,52 @@ from spotipy.oauth2 import SpotifyOAuth
 import spotipy
 import yaml
 
-config_root = Path('~/.config/spotidry').expanduser().resolve()
-with open(config_root.joinpath('spotidry.yaml'), 'r') as stream:
-    try:
-        config = yaml.safe_load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
+class Spotidry():
+    '''
+    Spotify status/commands
+    '''
 
-sp = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(
-        client_id=config.get('client_id'),
-        client_secret=config.get('client_secret'),
-        redirect_uri=config.get('redirect_uri'),
-        scope='user-read-currently-playing user-library-read user-library-modify',
-        cache_path=config_root.joinpath('.cache')))
+    def load_config(self):
+        config_root = Path('~/.config/spotidry').expanduser().resolve()
+        with open(config_root.joinpath('spotidry.yaml'), 'r') as stream:
+            try:
+                self.config = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+        self.cache_path = config_root.joinpath('.cache')
 
-track = sp.current_user_playing_track()
-# print(track)
-if not track:
-    print('No track currently playing')
-    exit()
+    def connect(self):
+        self.sp = spotipy.Spotify(
+            auth_manager=SpotifyOAuth(
+                client_id=self.config.get('client_id'),
+                client_secret=self.config.get('client_secret'),
+                redirect_uri=self.config.get('redirect_uri'),
+                scope='user-read-currently-playing user-library-read user-library-modify',
+                cache_path=self.cache_path))
 
-artist = track['item']['artists'][0]['name']
-song = track['item']['name']
-play_status = '▶' if track['is_playing'] else '⏸'
+    def get_status(self):
+        track = self.sp.current_user_playing_track()
 
-track_id = track['item']['id']
-liked_status = '❤' if sp.current_user_saved_tracks_contains(tracks=[track_id])[0] else '♡'
+        if not track:
+            print(' ⏹')
+            exit()
 
-print(f'{play_status} {artist} - {song} {liked_status}')
+        artist = track['item']['artists'][0]['name']
+        song = track['item']['name']
+        play_status = '▶' if track['is_playing'] else '⏸'
 
-# sp.current_user_saved_tracks_delete(tracks=[track_id])
-# sp.current_user_saved_tracks_add(tracks=[track_id])
+        track_id = track['item']['id']
+        liked_status = '❤' if self.sp.current_user_saved_tracks_contains(
+            tracks=[track_id])[0] else '♡'
+
+        print(f'{play_status} {artist} - {song} {liked_status}')
+
+        # sp.current_user_saved_tracks_delete(tracks=[track_id])
+        # sp.current_user_saved_tracks_add(tracks=[track_id])
+
+    def __init__(self, **kwargs):
+        '''
+        Initialize all the variables
+        '''
+        self.load_config()
+        self.connect()
