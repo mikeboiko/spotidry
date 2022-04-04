ceff6f80f5a4383d8dc8220e47eabe79f4f347a6
Mike Boiko <michaelboiko@gmail.com>
2022-04-04 08:29:59 -0600

    Reved up to 0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.0.2 (2022-04-04)
+
+- Updated instructions
+
 # 0.0.1 (2022-04-03)
 
 - Initial release
--- spotidry/__init__.py
+++ spotidry/__init__.py
@@ -1,7 +1,7 @@
 '''
-spotipy
+spotidry
 '''
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'Mike Boiko'
 __email__ = 'mike@boiko.ca'
